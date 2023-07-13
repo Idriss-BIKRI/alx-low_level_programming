7b8769fde7f4fc200f14d1f60135b866a3ab329f
idriss <idrissbikri@gmail.com>
2023-07-13 16:28:55 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-	const char *message = "\"Programming is like building a multilingual puzzle";
-	puts(message);
-
+	puts("Programming is like building a multilingual puzzle);
 	return 0;
 }		
