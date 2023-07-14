37cf8139cfe7bc72ff2fa9fb659d3ce6cfbf556a
idriss <idrissbikri@gmail.com>
2023-07-14 15:46:39 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *main - Putchar
+  *
+  *Return: 0 (success)
+  *
+  */
+int main(vod)
+{
+	char ch;
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch)
+	}
+	return (0);
