88264cc10111d9187b12752a61d56d7747d09695
idriss <idrissbikri@gmail.com>
2023-07-14 13:13:37 -0700

    0x00. C - Hello, World

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,10 +14,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
+        {
 		printf("Last digit of 98 is 8 and is greater than 5\n");
+        }         
 	else if (n == 0)
+        { 
 		printf("Last digit of 980 is 0 and is 0\n");
-	if (n < 6)
+        }
+	else if (n < 6)
+        {
 		printf("Last digit of -98 is -8 and is less than 6 and not 0\n");
+        }       
 	return (0);
 }
