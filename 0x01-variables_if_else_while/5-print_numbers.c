fafb4aecfe79433fc302ea2dc1141207958d3224
idriss <idrissbikri@gmail.com>
2023-07-14 17:47:40 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main -putchar
+  *
+  *Return: 0 (success)
+  *
+  */
+int main(void)
+{
+	int num;
+	for (num = '0' ; num <= '9' ; num++)
+{
+	putchar(num);
+}
+	putchar('\n');
+	return (0);
+}
