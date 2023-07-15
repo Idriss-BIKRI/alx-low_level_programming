c2caa93c83c47fc0b603eae894e67ffb5064fcbb
idriss <idrissbikri@gmail.com>
2023-07-14 17:11:53 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main-putchar
+  *
+  *Return: 0 (success)
+  *
+  */
+int main(void)
+{
+	char ch;
+
+	for(ch = 'q' ; ch <= 'z' ; ch++) {
+		if (ch != 'q' && ch != 'e') {
+		putchar(ch);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
