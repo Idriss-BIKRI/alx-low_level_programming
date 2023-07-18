c142513160db6ad6bdfa8dc56307f24084e7285b
idriss <idrissbikri@gmail.com>
2023-07-18 10:45:00 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+ #include "main.h"
+/**
+  *main - _putchar
+  *
+  *Return: 0 (success).
+  *
+  */
+int main(void)
+{
+	char ch;
+	
+	for ( ch = 'a'; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
