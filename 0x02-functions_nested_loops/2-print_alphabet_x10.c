4bc77f0e07e2279bac2c02fb498fc8befee33726
idriss <idrissbikri@gmail.com>
2023-07-18 11:12:42 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+include "main"
+/**
+  *print_alphabet_x10 - preint alphabet in 10 lines
+  *
+  *Retune: always 0.
+  *
+  */
+void print_alphabet_x10(void)
+{
+	int ln;
+	char ch;
+
+	for (ln = 1 ; ln <= 10 ; ln++)
+
+	{
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+		{
+			_putchar("ch");
+		}
+
+			_putchar('\n');
+	}
