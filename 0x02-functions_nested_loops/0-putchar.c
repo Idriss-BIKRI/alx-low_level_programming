8878b838bc770dae31d1bb0a9595fd8aaccc8a15
idriss <idrissbikri@gmail.com>
2023-07-18 10:19:28 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
   */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	-putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t';
+	_putchar('c');
+	_putchar('h');
+	-putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
