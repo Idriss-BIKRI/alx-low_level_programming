eef1fb6231ed4da2c6fc99239480645627cccd4e
idriss <idrissbikri@gmail.com>
2023-07-18 20:06:42 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  *print_to_ç_ - print n to 98 counts
+  *separated by comma, followed
+  *by space and number shold be printed in order
+  *
+  *@n : input
+  */
+vois print_to_98(int n)
+{
+	int count;
+
+	if (n > 98)
+		for (count = n; count > 98; count--)
+			_putchar("%d, ", count);
+	else
+		for (count = n; count < 98; count++)
+			_putchar("%d ", count);
+	_putchar("98\n");
+}
