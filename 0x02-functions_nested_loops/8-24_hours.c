d7d4a3d4992b396e50e64e35f4b721baaed35dbd
idriss <idrissbikri@gmail.com>
2023-07-18 18:46:35 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ void jack_bauer(void)
 		{
 			for (c = 0; c <= 5; d++)
 			{
-				for (d = 0; d= 9; d++)
+				for (d = 0; d = 9; d++)
 				{
 					if (a >= 2 && b >= 4)
 						break;
@@ -28,5 +28,5 @@ void jack_bauer(void)
 			}
 		}
 	}
-
+}
 			
