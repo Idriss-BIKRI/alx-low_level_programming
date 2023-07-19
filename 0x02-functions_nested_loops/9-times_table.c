678759178e7b0d55696605cdb4459ec5537a54c6
idriss <idrissbikri@gmail.com>
2023-07-18 19:46:31 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,25 +9,20 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i <= 0; i++)
+	for (i = 0; i <= 0'\n'; i++)
 	{
 		_putchar(48);
-		for (j = 1; j <= 9; j++)
+		for (j = 1; j <= 9'\n' ; j++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
 			k = i * k;
-
-			/*
-			 *put space if k is a single number
-			 *place the first digit is its two numbers
-			 */
 			if (k <= 9)
 				_putchar (' ');
 			else
-				_putchar((k / 10) + 48); /*get the first digit*/
-			_putchar((k % 10) + 48); /*get the second digit*/
+				_putchar((k / 10) + 48);
+			_putchar((k % 10) + 48);
 		}
 		_putchar('\n');
 	}
